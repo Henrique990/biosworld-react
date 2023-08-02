13b51064d121dfd0a6132cd243d62f5ab3db2fc1
Henrique Teixeira <henriqueteixeira26@gmail.com>
2023-08-02 17:19:19 -0300

    FAQs update meme

--- src/components/Faqs/index.jsx
+++ src/components/Faqs/index.jsx
@@ -6,284 +6,167 @@ export default function Faqs() {
   return (
     <section className="">
       <div className="container px-6 py-12 mx-auto">
-        <h1 className="text-3xl my-10 font-['Cattino'] text-center font-semibold text-green-400 lg:text-5xl dark:text-green-400">Frequently Asked Questions (FAQs)</h1>
-        <h2 className="text-2xl font-['Cattino'] text-center font-semibold text-gray-800 lg:text-3xl dark:text-white">
+        <h1 className="text-3xl my-10 font-['Cattino'] text-center font-semibold text-green-400 lg:text-5xl ">Frequently Asked Questions (FAQs)</h1>
+        <h2 className="text-2xl font-['Cattino'] text-center font-semibold text-gray-800 lg:text-3xl ">
           {t("faq-title")}
         </h2>
 
         <div className="grid grid-cols-1 gap-8 mt-8 lg:mt-16 md:grid-cols-2 xl:grid-cols-3">
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+            <div className="inline-block p-3 text-white rounded-lg">
+                <img className="w-6" src="meme-svg/12 1.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-1")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer1")}
               </p>
             </div>
           </div>
 
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+            <div className="inline-block p-3 text-white rounded-lg">
+            <img className="w-6" src="meme-svg/deer 1.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-2")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-2")}
               </p>
             </div>
           </div>
 
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+          <div className="inline-block p-3 text-white rounded-lg">
+            <img className="w-6" src="meme-svg/fox 1.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-3")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-3")}
               </p>
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-3-tier1")}
               </p>
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-3-tier2")}
               </p>
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-3-tier3")}
               </p>
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-3-tier4")}
               </p>
             </div>
           </div>
 
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+          <div className="inline-block p-3 text-white rounded-lg">
+            <img className="w-6" src="meme-svg/Group 100.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-4")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-4")}
               </p>
             </div>
           </div>
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+          <div className="inline-block p-3 text-white rounded-lg">
+            <img className="w-6" src="meme-svg/Group 109.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-5")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-5")}
               </p>
             </div>
           </div>
 
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+          <div className="inline-block p-3 text-white rounded-lg">
+            <img className="w-6" src="meme-svg/Group 110.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-6")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-6")}
               </p>
             </div>
           </div>
 
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+          <div className="inline-block p-3 text-white rounded-lg">
+            <img className="w-8" src="meme-svg/Group 111.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-7")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-7")}
               </p>
             </div>
           </div>
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+          <div className="inline-block p-3 text-white rounded-lg">
+            <img className="w-6" src="meme-svg/Group 112.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-8")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-8")}
               </p>
             </div>
           </div>
           <div>
-            <div className="inline-block p-3 text-white bg-green-600 rounded-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+          <div className="inline-block p-3 text-white rounded-lg">
+            <img className="w-6" src="meme-svg/owle 1.svg" alt="porco espinho" />
             </div>
 
             <div>
-              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 dark:text-white">
+              <h1 className="text-xl font-['Cattino'] font-semibold text-gray-700 ">
                 {t("faq-question-9")}
               </h1>
 
-              <p className="mt-2 font-semibold text-sm text-gray-500 dark:text-gray-300">
+              <p className="mt-2 font-semibold text-sm text-gray-500 ">
                 {t("faq-answer-9")}
               </p>
             </div>
           </div>
         </div>
         <div className="mt-10">
-          <p className="mt-2 text-center font-bold text-sm text-gray-500 dark:text-gray-300">
+          <p className="mt-2 text-center font-bold text-sm text-gray-500 ">
             {t("faq-text-finally")}
           </p>
         </div>
